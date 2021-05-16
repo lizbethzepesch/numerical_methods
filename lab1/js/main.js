7adc95ea6627330c0d6445db5f706d06ae80c252
lizbethzepesch <elizbetti15@gmail.com>
2021-05-17 01:52:01 +0300

    some of lab1

--- lab1/js/main.js
+++ lab1/js/main.js
@@ -19,3 +19,35 @@ function check(a, b){
         if (!b.includes(i))
             B.push(i.toUpperCase())
 }
+
+
+
+
+function entry(matrixA, matrixB){
+    for (let i of matrixA)
+       if (!matrixB.includes(i))
+           return 'False'
+
+    return 'True'
+}
+
+function union(matrixA, matrixB){
+    res = createCopy(matrixA)
+    for (let i of matrixB)
+        if (!res.includes(i))
+            res.append(i)
+    return res
+}
+
+
+function main(){
+    myA = [21, 9, 10, 7, 1, 2, 3, 3, 1, 7]
+    myB = [21, 9, 10, 7, 0, 5, 14, 14, 9 ,10]
+    myA1 = [21, 9, 10, 7, 1, 2, 3]
+    myB1 = [21, 9, 10, 7, 0, 5, 14]
+    myA, myB = check_input(myA, myB)
+    console.log(myA)
+    console.log(myB)
+}
+
+main()
