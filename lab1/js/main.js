452da0df7028dbb30449be0669e5a0c7b40d8497
Yelyzaveta Kliuieva <elizbetti15@gmail.com>
2021-05-17 15:27:31 +0300

    intersection

--- lab1/js/main.js
+++ lab1/js/main.js
@@ -1,19 +1,36 @@
-
-
 function symmetric_difference(matrixA, matrixB){
-    let temp = union(matrixA, matrixB)
-    let res = []
-    for (let i = 0; i < temp.length; i++)
-        if (!matrixA.includes(temp[i]) || matrixB.includes(temp[i]))
-            res.push[i]
+    // let temp = union(matrixA, matrixB)
+    // let res = []
+    // for (let i = 0; i < temp.length; i++)
+    //     if (!matrixA.includes(temp[i]) || matrixB.includes(temp[i]))
+    //         res.push[i]
 
-    return res
+    // return res
 }
 
-function delDupl(set){
+function intersection(matrixA, matrixB){
+    let res = []
+    matrixA.filter((value, index, arr) => arr.indexOf(value) === index)
+    matrixB.filter((value, index, arr) => arr.indexOf(value) === index)
+    matrixA.sort()
+    matrixB.sort()
+
+    for (let i = 0, k = 0, j = 0; i < matrixA.length && j < matrixB.length; ) 
+		if (matrixA[i] == matrixB[j]) { 
+			res[k] = matrixA[i];
+			k++, i++, j++
+		} 
+        else 
+			if (matrixA[i] < matrixB[j]) i++
+            else j++
+	
+    return res;
+}
+
+function fixInput(set){
     let res = []
     for (let i = 0; i < set.length; i++)
-        if(!res.includes(set[i].toUpperCase()) && set[i].length === 1 && set[i].match(/[a-z]/i))
+        if(!res.includes(set[i].toUpperCase()) && set[i].length === 1 && set[i].match(/[matrixA-z]/i))
             res.push(set[i].toUpperCase())
     return res
 }
@@ -40,7 +57,8 @@ function union(matrixA, matrixB){
 }
 
 function toRes(arr){
-    let res = '[' + arr[0] 
+    let res = '[' 
+    res += arr[0] 
     for(let i = 1; i < arr.length; i++)
         res += ', ' + arr[i]
     
@@ -48,12 +66,12 @@ function toRes(arr){
 }
 
 function solve(){
-    let matrixA = delDupl(document.querySelector('.A').value.split(" "))
-    document.querySelector('.mA').innerHTML += toRes(matrixA)
-    let matrixB = delDupl(document.querySelector('.B').value.split(" "))
-    document.querySelector('.mB').innerHTML += toRes(matrixB)
+    let matrixA = fixInput(document.querySelector('.A').value.split(" "))
+    document.querySelector('.mA').innerHTML = 'A:' + toRes(matrixA)
+    let matrixB = fixInput(document.querySelector('.B').value.split(" "))
+    document.querySelector('.mB').innerHTML = 'B:' + toRes(matrixB)
 
-    let func 
+
     switch(document.querySelector("#select").value){
         case "union":
             document.querySelector('.res').textContent = toRes(union(matrixA, matrixB))
@@ -62,17 +80,17 @@ function solve(){
             document.querySelector('.res').textContent = entry(matrixA, matrixB)
             break
         case "symmetric difference":
-            func = symmetric_difference
+            document.querySelector('.res').textContent = toRes(symmetric_difference(matrixA, matrixB))
             break
         case "difference":
-            func = difference
+            document.querySelector('.res').textContent = toRes(difference(matrixA, matrixB))
             break
         case "intersection":
-            func = intersection
+            document.querySelector('.res').textContent = toRes(intersection(matrixA, matrixB))
             break
     }
+
     document.querySelector('#result').style.display = 'unset'
-    
 }
 // F G J Q
 // L F H J I O P E
