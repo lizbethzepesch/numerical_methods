8b2c9c3222e8105c6d823d9520dd73a7b9671e7f
lizbethzepesch <elizbetti15@gmail.com>
2021-05-17 04:25:42 +0300

    some of lab1

--- lab1/js/main.js
+++ lab1/js/main.js
@@ -1,53 +1,78 @@
-var A = []
-var B = []
 
-function createCopy(A) {
-    let copy = []
-    for (const arr of A) 
-        copy.push([...arr])
-    
-    return copy
+
+function symmetric_difference(matrixA, matrixB){
+    let temp = union(matrixA, matrixB)
+    let res = []
+    for (let i = 0; i < temp.length; i++)
+        if (!matrixA.includes(temp[i]) || matrixB.includes(temp[i]))
+            res.push[i]
+
+    return res
 }
 
-function check(a, b){
-    A = []
-    B = []
-    for (let i = 0; i < a.length; i++)
-        if (!a.includes(i))
-            A.push(i.toUpperCase())
-    for (let i = 0; i < b.length; i++)
-        if (!b.includes(i))
-            B.push(i.toUpperCase())
+function delDupl(set){
+    let res = []
+    for (let i = 0; i < set.length; i++)
+        if(!res.includes(set[i].toUpperCase()) && set[i].length === 1 && set[i].match(/[a-z]/i))
+            res.push(set[i].toUpperCase())
+    return res
 }
-
-
-
 
 function entry(matrixA, matrixB){
-    for (let i of matrixA)
-       if (!matrixB.includes(i))
+    for (let i = 0; i < matrixA.length; i++)
+       if (!matrixB.includes(matrixA[i])){
+           console.log(matrixA[i])
            return 'False'
+        }
 
     return 'True'
 }
 
 function union(matrixA, matrixB){
-    res = createCopy(matrixA)
-    for (let i of matrixB)
-        if (!res.includes(i))
-            res.append(i)
+    let res = []
+    for (let i = 0; i < matrixA.length; i++)
+        res.push(matrixA[i])
+
+    for (let i = 0; i < matrixB.length; i++)
+        if (!res.includes(matrixB[i]))
+            res.push(matrixB[i])
     return res
 }
 
-
-function main(){
-    myA = [21, 9, 10, 7, 1, 2, 3, 3, 1, 7]
-    myB = [21, 9, 10, 7, 0, 5, 14, 14, 9 ,10]
-    myA1 = [21, 9, 10, 7, 1, 2, 3]
-    myB1 = [21, 9, 10, 7, 0, 5, 14]
-    myA, myB = check_input(myA, myB)
-    console.log(myA)
-    console.log(myB)
+function toRes(arr){
+    let res = '[' + arr[0] 
+    for(let i = 1; i < arr.length; i++)
+        res += ', ' + arr[i]
+    
+    return res + ']'
 }
 
-main()
+function solve(){
+    let matrixA = delDupl(document.querySelector('.A').value.split(" "))
+    document.querySelector('.mA').innerHTML += toRes(matrixA)
+    let matrixB = delDupl(document.querySelector('.B').value.split(" "))
+    document.querySelector('.mB').innerHTML += toRes(matrixB)
+
+    let func 
+    switch(document.querySelector("#select").value){
+        case "union":
+            document.querySelector('.res').textContent = toRes(union(matrixA, matrixB))
+            break
+        case "entry":
+            document.querySelector('.res').textContent = entry(matrixA, matrixB)
+            break
+        case "symmetric difference":
+            func = symmetric_difference
+            break
+        case "difference":
+            func = difference
+            break
+        case "intersection":
+            func = intersection
+            break
+    }
+    document.querySelector('#result').style.display = 'unset'
+    
+}
+// F G J Q
+// L F H J I O P E
